92696a62049f2efe95d6ac9eb017eb05d6a6cab1
shch <shch.pc@gmail.com>
2018-09-05 19:05:30 +0000

    save

---
+++ client/views/header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header-container">
+        <ul>
+            <li>Head</li>
+            <li><router-link to="/">Top</router-link></li>
+            <li><router-link to="/login">Test</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+ul {
+    padding: 0;
+    margin: 0;
+}
+li {
+    display: inline-block;
+
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "head"
+});
+</script>
