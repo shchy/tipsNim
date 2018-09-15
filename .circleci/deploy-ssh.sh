92e9e3628396ab3ff264d7f6d957f990913bfb71
shch <shch.pc@gmail.com>
2018-09-15 16:28:58 +0000

    mod .circleci

--- .circleci/deploy-ssh.sh
+++ .circleci/deploy-ssh.sh
@@ -1,6 +1,6 @@
 expect -c "
     set timeout 30
-    spawn ssh root@$digital_ip 'bash -s' < ./deploy.sh
+    spawn ssh root@$digital_ip 'bash -s' < .circleci/deploy.sh
     expect ":"
     send \"$digital_pass\n\"
     interact
