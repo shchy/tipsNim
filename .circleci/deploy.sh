0735e41340e12ff82df14789386db4e6f475d5de
shch <shch.pc@gmail.com>
2018-09-15 17:12:45 +0000

    mod .circleci

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,12 +1,10 @@
-echo "stop docker container" 
-docker ps -a -q | xargs docker stop 
-docker ps -a -q | xargs docker rm
-
-echo clean
-rm -rf tipsNim/
-
-echo build
-git clone https://github.com/shchy/tipsNim.git
-cd tipsNim
-docker build -t tips .
+echo "stop docker container" \
+docker ps -a -q | xargs docker stop \
+docker ps -a -q | xargs docker rm \
+echo clean \
+rm -rf tipsNim/ \
+echo build \
+git clone https://github.com/shchy/tipsNim.git \
+cd tipsNim \
+docker build -t tips . \
 docker run -p 80:8080 -d tips
