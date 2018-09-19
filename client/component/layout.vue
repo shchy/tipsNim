f75a04326930a6b155601f7614cddaafadfa6fba
shch <shch.pc@gmail.com>
2018-09-19 13:31:46 +0900

    menu icon size

--- client/component/layout.vue
+++ client/component/layout.vue
@@ -5,7 +5,7 @@
         </nav>
         <div class="content">
             <header>
-                <a id="side-open" href="#" @click="toggleMenu"><i class="fas fa-bars fa-2x"></i></a>
+                <a id="side-open" @click="toggleMenu"><i class="fas fa-bars"></i></a>
                 <router-view id="header-content" name="header"></router-view>
             </header>
             <main class="main">
@@ -19,110 +19,107 @@
 import Vue from "vue";
 
 export default Vue.extend({
-    data() {
-        return {
-            isOpenMenu: false
-        }
-    },
-    methods:{
-        toggleMenu: function (){
-            this.isOpenMenu = !this.isOpenMenu;
-        }
-    },
-    watch:{
-        $route (to, from){
-            this.isOpenMenu = false;
-        }
-    } 
+  data() {
+    return {
+      isOpenMenu: false
+    };
+  },
+  methods: {
+    toggleMenu: function() {
+      this.isOpenMenu = !this.isOpenMenu;
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.isOpenMenu = false;
+    }
+  }
 });
-
 </script>
 
 
 <style scoped>
 #layout {
-    height: 100vh;
-    display: flex;
-    
-    
-    /*background-image: url(https://www.webfx.com/blog/images/assets/cdn.sixrevisions.com/0431-01_responsive_background_image_demo/images/background-photo.jpg);
+  height: 100vh;
+  display: flex;
+
+  /*background-image: url(https://www.webfx.com/blog/images/assets/cdn.sixrevisions.com/0431-01_responsive_background_image_demo/images/background-photo.jpg);
     background-position: center center;
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-size: cover;
     background-color: #464646;*/
 }
-#layout[isOpenMenu] .side-nav{
-    width: 200px;
-    transition: width 300ms 0s ease;
+#layout[isOpenMenu] .side-nav {
+  width: 200px;
+  transition: width 300ms 0s ease;
 }
-#layout[isOpenMenu] .content{
-    transform: translateX(200px);
-    transition: transform 300ms 0s ease;
+#layout[isOpenMenu] .content {
+  transform: translateX(200px);
+  transition: transform 300ms 0s ease;
 }
 
-.side-nav{     
-    position: absolute;
-    height: 100vh;
-    background: #fff;
-    
-    overflow-y: scroll;
-    order: -1;
-    
-    width: 0px;
-    
-    display: flex;
-    flex-direction: column;
-    transition: width 200ms 0s ease;
-    
+.side-nav {
+  position: absolute;
+  height: 100vh;
+  background: #fff;
+
+  overflow-y: scroll;
+  order: -1;
+
+  width: 0px;
+
+  display: flex;
+  flex-direction: column;
+  transition: width 200ms 0s ease;
 }
 
 #side-close {
-    align-self: flex-end;
-    padding-top: 1rem;
-    padding-right: 1rem;
+  align-self: flex-end;
+  padding-top: 1rem;
+  padding-right: 1rem;
 }
 
 #side-open {
-    width: auto;
-    margin-right: 1rem;
+  width: auto;
+  margin-right: 1rem;
+  cursor: pointer;
 }
 
 #side-content {
-    padding: 1rem;
-    overflow-y: scroll;
-    -webkit-overflow-scrolling: touch;
+  padding: 1rem;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
 }
 
-.content{
-    flex: 1;
-    display: flex;
-    flex-direction: column-reverse;
-    
-    transition: transform 200ms 0s ease;
+.content {
+  flex: 1;
+  display: flex;
+  flex-direction: column-reverse;
+
+  transition: transform 200ms 0s ease;
 }
 
-header{
-    height: auto;
-    display: flex;
-    padding: 1rem;
-    justify-content: center;
-    align-items: center;
+header {
+  height: auto;
+  display: flex;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
 }
 
 #header-content {
-    flex: 1;
+  flex: 1;
 }
 
 .main {
-    flex: 1;
-    overflow-y: scroll;
-    -webkit-overflow-scrolling: touch;
+  flex: 1;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
 }
 
 @media screen and (min-width: 768px) {
-
-    /*
+  /*
     .side-nav{
         position: relative;
         width: 200px;
@@ -133,9 +130,8 @@ header{
     #side-content {
         padding-top: 1rem;
     }*/
-    .content{
-        flex-direction: column;
-    }
-   
+  .content {
+    flex-direction: column;
+  }
 }
 </style>
--- client/views/header.vue
+++ client/views/header.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="header-container">
         <ul>
-            <li><router-link to="/"><i class="fas fa-home fa-2x"></i></br>Home</router-link></li>
-            <li><router-link to="/login">Test</router-link></li>
+            <li><router-link to="/"><i class="fas fa-home"></i><span>Home</span></router-link></li>
+            <li><router-link to="/login"><span>Test</span></router-link></li>
         </ul>
     </div>
 </template>
 
 <style scoped>
 ul {
-    padding: 0;
-    margin: 0;
+  padding: 0;
+  margin: 0;
 }
 li {
-    display: inline-block;
-    text-align: center;
-    margin: 0rem 0.5rem;
+  display: inline-block;
+  text-align: center;
+  margin-left: 0.5rem;
+  line-height: 0.5em;
 }
 
+span::before {
+  content: "\A";
+  white-space: pre;
+}
+span {
+  font-size: 0.5em;
+}
 </style>
 
 <script lang="ts">
