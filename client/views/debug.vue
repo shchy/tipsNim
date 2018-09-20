5c2b6cf351daf65d037f34b0c834354011b3a0b8
shch <shch.pc@gmail.com>
2018-09-20 15:57:23 +0000

    save

--- client/views/debug.vue
+++ client/views/debug.vue
@@ -13,12 +13,12 @@
 #carousel {
   /* Ensure that the contents flow horizontally */
   overflow-x: auto;
-  white-space: nowrap;
+  /* white-space: nowrap; */
   display: flex;
 }
 
 #carousel {
-  position: relative;
+  /* position: relative; */
   width: 100%;
   height: 100%;
 }
@@ -29,6 +29,15 @@
   -webkit-overflow-scrolling: touch; /* Needed to work on iOS Safari */
 }
 
+/* 2015 spec - For Firefox, Edge, IE */
+/* #carousel.snap {
+  scroll-snap-type: mandatory;
+  -ms-scroll-snap-type: mandatory;
+  scroll-snap-points-x: repeat(100%);
+  -ms-scroll-snap-points-x: repeat(100%);
+} */
+
+
 #carousel.snap > div {
   scroll-snap-align: center;
 }
@@ -36,29 +45,21 @@
 #carousel > div {
   min-width: 100%;
   min-height: 100%;
-  display: flex;
+  /* display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
   color: #fff;
-  font-size: 20px;
+  font-size: 20px; */
 }
 
-/* 2015 spec - For Firefox, Edge, IE */
-#carousel.snap {
-  scroll-snap-type: mandatory;
-  -ms-scroll-snap-type: mandatory;
-  scroll-snap-points-x: repeat(100%);
-  -ms-scroll-snap-points-x: repeat(100%);
-}
 
 /* Below here is styling, not important for the actual example, just to make it look nice. No need to look down here! */
 
 #debug-container {
-  height: 100%;
+  /* height: 100%; */
   margin: 0;
   padding: 0;
-  background: #226de2;
 }
 
 
