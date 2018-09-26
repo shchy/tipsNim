8dc8c260d912a33607ce7f42f573164278221e08
shch <shch.pc@gmail.com>
2018-09-26 13:47:03 +0000

    add favicon

--- client/main.ts
+++ client/main.ts
@@ -5,6 +5,10 @@ import layout from '@/component/layout.vue'
 import '@/css/reboot.css'
 import '@/css/base.css'
 
+import smoothscroll from 'smoothscroll-polyfill';
+
+smoothscroll.polyfill();
+
 new Vue({
     router,
     components: {
