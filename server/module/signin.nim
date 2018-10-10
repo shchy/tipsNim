852ce19bac96667dfac10d3e630e6e5cf3edb740
shch <shch.pc@gmail.com>
2018-10-10 13:52:18 +0900

    resolve warning

--- server/module/signin.nim
+++ server/module/signin.nim
@@ -9,7 +9,7 @@ import
 let signin = handler(ctx) do:
     let id = ctx.req.getFormParam "id"
     let pass = ctx.req.getFormParam "password"
-    let isRemember = ctx.req.getFormParam "isRemember"
+    # let isRemember = ctx.req.getFormParam "isRemember"
     if ctx.signin(id, pass):
         return ctx.redirect("/home/")
     return ctx.redirect("/")
