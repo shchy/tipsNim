f627a71b2ae5ab0f583346e37af3340c40006c6a
shch <shch.pc@gmail.com>
2018-10-18 15:00:03 +0900

    save

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,11 +1,8 @@
-echo "stop docker container" \
-&& for container in $(docker ps -a -q); do (docker stop $container); done \
-&& for container in $(docker ps -a -q); do (docker rm $container); done \
-&& for container in $(docker image -q); do (docker image rm $container); done \
-&& echo clean \
+echo clean \
 && rm -rf tipsNim/ \
 && echo build \
 && git clone https://github.com/shchy/tipsNim.git \
 && cd tipsNim \
+&& sh .circleci/deploy_before.sh \
 && docker build -t tips . \
 && docker run -p 80:8000 -d tips
